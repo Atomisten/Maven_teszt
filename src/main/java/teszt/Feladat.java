fd6120f353277fc7be88da354b1ac2a3503afe85
Atomisten <rendesmail@gmail.com>
2022-02-04 11:45:58 +0100

    MAVEN prepared

--- src/main/java/teszt/Feladat.java
+++ src/main/java/teszt/Feladat.java
@@ -3,13 +3,17 @@ package teszt;
 public class Feladat {
 
     /*
-    Feladat:
-1, Hozzunk létre egy adatbázist country_app név alatt.
-2, Hozzunk létre metódust ami létrehozza a táblát a fejlécben látható oszlopnevekkel. + legyen ID oszlop ami auto increment.
-3, Hozzunk létre egy metódust ami létrehozza a recordot. pl.:  1,Spain,Barcelona,1600000
-4, Hozzunk létre egy metódust amivel updatelni tudunk egy recordot egy bizonyos id alapján.
-5, Hozzunk létre egy metódust ami visszaadja az adott IDhoz tartozo recordot.
-6, Hozzunk létre egy metódust amivel visszaadja az összes rekordot a táblából.
+1, Hozzunk létre dog_shelter adatbazázist.
+2, Csináljunk egy metódust ami megcsinálja a dogs táblánkat.
+3, Csináljunk egy metódust ami elmenti a data.txt -ből az adatainkat a táblázatba.
+4, Csináljunk egy metódust ami paraméterként bekér egy booleant és azokat a kutyákat húzza fel az adatbázisbol ami a friendly oszlop megegyezik.
+5, Csináljunk egy metódust ami ID alapján felhúz egy kutya adatot.
+6, Csináljunk egy metódust ami felhúzza az összes kutyát.
+7, Csináljunk egy metódust ami ID alapján tudja módostani a kutya életkorát
+8, Csináljunk egy metódust
 
+EXTRA:
+Csináljunk egy metódust ami kér 2 számot paraméterként. Az egyik a limit lesz a másik az offset. Kérjünk ki mindig annyi kutyát ami a limit
+és hagyjunk ki annyi kutyát ami az offset
      */
 }
--- src/main/java/teszt/MavenTeszt.java
+++ src/main/java/teszt/MavenTeszt.java
@@ -5,112 +5,158 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MavenTeszt {
 
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/country_app?useSSL=false&serverTimezone=UTC";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/dog_shelter?createDatabaseIfNotExist=true";
     public static final String USER = "root";
     public static final String PASSWORD = "Test123!";
-    static String[] oszlopok;
+//    static String[] oszlopok;
 
     public static void main(String[] args) {
-        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get("src/main/resources/data.txt"))) {
+//        createDB();
+//        createTable(columnNames().get(0)[0], columnNames().get(0)[1], columnNames().get(0)[2], columnNames().get(0)[3]);
+//        createDogs(columnNames());
+//        friendlyDogs(true);
+//        idDog(1);
+//        allDog();
+//        updateDog(10, 1);
+        allDogLimit(3,2);
+    }
+
+
+    private static List<String[]> columnNames() {
+        List<String[]> oszlopok = new ArrayList<>();
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get("src/main/resources/data2.txt"))) {
             String line;
-            String oszlopnevek;
-            oszlopnevek = bufferedReader.readLine();
-            oszlopok = oszlopnevek.split(";");
             while ((line = bufferedReader.readLine()) != null) {
-                String[] fields = line.split(";");
-                String country = fields[0];
-                String city = fields[1];
-                int population = Integer.parseInt(fields[2]);
-//                createCountry(country, city, population);
+                String[] data = line.split(";");
+                oszlopok.add(data);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-//        createTable();
-//        updateCountry(8);
-//        giveInfoFromCountry(8);
-        giveAllInfo();
+        return oszlopok;
     }
 
-    public static void createTable() {
+    public static void createDB() {
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
-            Statement statement = connection.createStatement(); // Serpa/Kocsi üres
+            Statement statement = connection.createStatement();
+//            statement.execute("CREATE DATABASE dog_shelter");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-            String createTable = "CREATE TABLE IF NOT EXISTS countries " +
-                    "(ID INT AUTO_INCREMENT PRIMARY KEY, " +
-                    oszlopok[0] + " VARCHAR(100), " +
-                    oszlopok[1] + " VARCHAR(255), " +
-                    oszlopok[2] + " INT); ";
-            statement.execute(createTable); // sql parancsot rárakjuk a kocsira
-
+    private static void createTable(String breed, String name, String age, String friendly) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+            String createTable = "CREATE TABLE IF NOT EXISTS dogs (" + "id INT PRIMARY KEY AUTO_INCREMENT, " + breed + " VARCHAR(100), " + name + " VARCHAR(100), " + age + " INT, " + friendly + " BOOLEAN); ";
+            PreparedStatement preparedStatement = connection.prepareStatement(createTable); //Kocsint/Serpa
+            preparedStatement.execute(createTable);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
-
-    public static void createCountry(String country, String city, int population) {
+    private static void createDogs(List<String[]> dogList) {
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
-            Statement statement = connection.createStatement(); // Serpa/Kocsi üres
+            for (int i = 1; i < dogList.size(); i++) {
+                String dogBreed = dogList.get(i)[0];
+                String dogName = dogList.get(i)[1];
+                int dogAge = Integer.parseInt(dogList.get(i)[2]);
+                boolean friendly = Boolean.parseBoolean(dogList.get(i)[3]);
+                String createCustomer = "INSERT INTO dogs (dog_breed, dog_name, dog_age, friendly) VALUES (?,?,?,?)"; // Query
+                PreparedStatement preparedStatement = connection.prepareStatement(createCustomer); //Kocsint/Serpa
+                preparedStatement.setString(1, dogBreed);
+                preparedStatement.setString(2, dogName);
+                preparedStatement.setInt(3, dogAge);
+                preparedStatement.setBoolean(4, friendly);
 
-            String createCountry =
-                    "INSERT INTO countries (country, city, population) " +
-                            "VALUES ('" + country + "', '" + city + "', " + population + ")";
-            statement.execute(createCountry); // sql parancsot rárakjuk a kocsira
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-
-    public static void updateCountry(int id) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
-            Statement statement = connection.createStatement(); // Serpa/Kocsi üres
-
-            String updateCountry =
-                    "UPDATE countries SET population = 4000000 WHERE id =" + id;
-            statement.execute("SET sql_safe_updates = 1");  // Safe update be es kikapcsolasa ezzel a paranccsal
-            int affectedRows = statement.executeUpdate(updateCountry); // sql parancsot rárakjuk a kocsira
-            System.out.println("Rows: " + affectedRows);
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-    private static void giveInfoFromCountry(int id) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
-            Statement statement = connection.createStatement(); // Serpa/Kocsi üres
-
-            String giveInfo = "SELECT country, city, population from countries WHERE ID = " + id;
-            ResultSet resultSet = statement.executeQuery(giveInfo); // sql parancsot rárakjuk a kocsira
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(1) + " | " + resultSet.getString(2) + " | " +
-                        resultSet.getInt(3));
+                int affectedRows = preparedStatement.executeUpdate(); //executeQuery
+                System.out.println("Affected rows: " + affectedRows);
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
-    private static void giveAllInfo() {
+    private static void friendlyDogs(boolean b) {
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
-            Statement statement = connection.createStatement(); // Serpa/Kocsi üres
+            String userInfoQuery = "SELECT * FROM dogs WHERE friendly = ?"; // Query
+            PreparedStatement preparedStatement = connection.prepareStatement(userInfoQuery); //Kocsint/Serpa
+            preparedStatement.setBoolean(1, b);
+            ResultSet resultSet = preparedStatement.executeQuery(); //executeQuery
 
-            String giveInfo = "SELECT * from countries";
-            ResultSet resultSet = statement.executeQuery(giveInfo); // sql parancsot rárakjuk a kocsira
             while (resultSet.next()) {
-                System.out.println(resultSet.getInt(1) + "" +
-                        " | " + resultSet.getString(2) + " | " +
-                        "" + resultSet.getString(3) + " | " +
-                        "" + resultSet.getInt(4));
+                System.out.println(resultSet.getInt("ID") + " " + resultSet.getString("dog_name")+ " " + resultSet.getBoolean("friendly"));
             }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private static void idDog(int id) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+            String userInfoQuery = "SELECT * FROM dogs WHERE ID = ?"; // Query
+            PreparedStatement preparedStatement = connection.prepareStatement(userInfoQuery); //Kocsint/Serpa
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery(); //executeQuery
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2)+ " " + resultSet.getString(3 )+ " " + resultSet.getBoolean(4));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private static void allDog() {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+            String userInfoQuery = "SELECT * FROM dogs"; // Query
+            PreparedStatement preparedStatement = connection.prepareStatement(userInfoQuery); //Kocsint/Serpa
+//            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery(); //executeQuery
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2)+ " " + resultSet.getString(3 )+ " " + resultSet.getBoolean(4));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private static void updateDog(int age, int id) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+            String updateCustomer = "UPDATE dogs SET dog_age =? WHERE id =?"; // Query
+            PreparedStatement preparedStatement = connection.prepareStatement(updateCustomer); //Kocsint/Serpa
+
+            preparedStatement.setInt(1, age); //Ertekadas a ?nek
+            preparedStatement.setInt(2, id); //Ertekadas a ?nek
+
+            int affectedRows = preparedStatement.executeUpdate(); //executeQuery
+            System.out.println("Affected rows: " + affectedRows);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+    private static void allDogLimit(int limit, int offset) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+            String userInfoQuery = "SELECT * FROM dogs LIMIT ? OFFSET ?"; // Query
+            PreparedStatement preparedStatement = connection.prepareStatement(userInfoQuery); //Kocsint/Serpa
+            preparedStatement.setInt(1, limit);
+            preparedStatement.setInt(2, offset);
+            ResultSet resultSet = preparedStatement.executeQuery(); //executeQuery
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2)+ " " + resultSet.getString(3 )+ " " + resultSet.getBoolean(4));
+            }
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
